192a76589eb3cf4c9366347cb4c6611b84a0d6e8
Tom Vidar Michalsen <tomvidm@gmail.com>
2017-08-09 22:41:07 +0200

    Added a function to print directory structure to confirm functionality

--- depsearch/directory_tree.py
+++ depsearch/directory_tree.py
@@ -19,6 +19,8 @@ class DirectoryTree:
 		self.mapDirectories()
 		self.assignDepth(self.rootDir, 0)
 
+		self.printInfo()
+
 	def mapDirectories(self, ignoreHiddenFiles = True):
 		for root, subdirs, files in os.walk(self.rootDir):
 			if not ignoreHiddenFiles:
@@ -34,7 +36,18 @@ class DirectoryTree:
 	def assignDepth(self, directory, currentDepth):
 		self.directoryMap[directory].depth = currentDepth
 		for subdir in self.directoryMap[directory].subdirs:
-			self.assignDepth(os.path.join(directory, subdir), currentDepth)
+			self.assignDepth(os.path.join(directory, subdir), currentDepth + 1)
+
+	def printInfo(self):
+		queue = [self.rootDir]
+		while not len(queue) == 0:
+			current = queue.pop()
+			currentDepth = self.directoryMap[current].depth
+			print '  '*currentDepth, self.directoryMap[current].dir
+			for subdir in self.directoryMap[current].subdirs:
+				queue.append(os.path.join(current, subdir))
+			for file in self.directoryMap[current].files:
+				print '  '*currentDepth, file
 
 
 			
